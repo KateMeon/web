037492707428b54c92417938fbce1e308e2d79ff
Екатерина Корнева <katerina03535@yandex.ru>
2024-02-27 17:40:54 +0700

    lab2 start

--- abouttrip/trips/views.py
+++ abouttrip/trips/views.py
@@ -4,14 +4,30 @@ from django.shortcuts import redirect
 from django.template.loader import render_to_string
 from django.shortcuts import render
 
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+
+
+class MyClass:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'abouttrip/index.html')
+    data = {'title': 'Главная страница',
+            'menu': menu,
+            'float': 28.56,
+            'lst': [1, 2, 'abc', True],
+            'set': {1, 1, 2, 3, 2, 5},
+            'dict': {'key_1': 'value_1', 'key_2': 'value_2'},
+            'obj': MyClass(10, 20),
+            }
+    return render(request, 'abouttrip/index.html', context=data)
 
 
 def about(request):
-    return render(request, 'abouttrip/about.html')
+    return render(request, 'abouttrip/about.html', {'title': 'О сайте', 'menu': menu})
 
 
 def countries(request, country_id):
