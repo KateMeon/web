f95b5512d72f49182a6b60d035958cdedb56dcf0
Екатерина Корнева <katerina03535@yandex.ru>
2024-03-13 12:03:47 +0700

    finish lab3

--- abouttrip/trips/models.py
+++ abouttrip/trips/models.py
@@ -3,28 +3,34 @@ from django.urls import reverse
 
 
 # Create your models here.
+class PublishedModel(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_published=Trips.Status.PUBLISHED)
+
+
 class Trips(models.Model):
-    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    class Status(models.IntegerChoices):
+        DRAFT = 0, 'Черновик'
+        PUBLISHED = 1, 'Опубликовано'
+
+    title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, db_index=True,
                             unique=True)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
+    objects = models.Manager()
+    published = PublishedModel()
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         ordering = ['-time_create']
         indexes = [
             models.Index(fields=['-time_create']),
         ]
-
-    def __str__(self):
-        return self.title
-
-
-class PublishedModel(models.Manager):
-    def get_queryset(self):
-        return super().get_queryset().filter(is_published=1)
