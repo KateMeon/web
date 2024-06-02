c435576637574ae7b9d688cba1db5a15ef32393d
Екатерина Корнева <katerina03535@yandex.ru>
2024-06-02 18:12:08 +0700

    lab8 p14 LogoutView

--- abouttrip/abouttrip/settings.py
+++ abouttrip/abouttrip/settings.py
@@ -131,4 +131,5 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
+LOGOUT_REDIRECT_URL = 'home'
 LOGIN_REDIRECT_URL = 'home'
--- abouttrip/users/urls.py
+++ abouttrip/users/urls.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('login/', views.LoginUser.as_view(), name='login'),
-    path('logout/', views.logout_user, name='logout'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
 app_name = "users"
